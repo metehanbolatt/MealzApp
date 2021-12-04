4b4a7eb3d130438dc8112a86d02526fe064fa938
metehanie <metehan.bolat.ie@gmail.com>
2021-12-05 01:01:54 +0300

    Expanding list items

--- app/src/main/java/com/metehanbolat/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/metehanbolat/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,16 +1,22 @@
 package com.metehanbolat.mealzapp.ui.meals
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,6 +37,7 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal: MealResponse) {
+    var isExpanded by remember { mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 2.dp,
@@ -38,7 +45,9 @@ fun MealCategory(meal: MealResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
+        Row(
+            modifier = Modifier.animateContentSize()
+        ) {
             Image(
                 painter = rememberImagePainter(meal.imageUrl),
                 contentDescription = "Meal Image",
@@ -49,13 +58,31 @@ fun MealCategory(meal: MealResponse) {
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
                     .padding(16.dp)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.h6
                 )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = meal.description,
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.subtitle2,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 10 else 2
+                    )
+                }
             }
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand Row Icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable { isExpanded = !isExpanded }
+                    .align(if (isExpanded) Alignment.Bottom else Alignment.CenterVertically)
+            )
         }
     }
 
