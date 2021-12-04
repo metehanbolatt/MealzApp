4901f8c2578b049b2562c1ca65d2d7431dd9c53c
metehanie <metehan.bolat.ie@gmail.com>
2021-12-05 01:49:38 +0300

    Animating the size of composable

--- app/src/main/java/com/metehanbolat/mealzapp/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/com/metehanbolat/mealzapp/ui/details/MealDetailsScreen.kt
@@ -1,34 +1,52 @@
 package com.metehanbolat.mealzapp.ui.details
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.metehanbolat.mealzapp.model.response.MealResponse
 
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+    val imageSizeDp: Dp by animateDpAsState(targetValue = if (isExpanded) 200.dp else 100.dp)
+
     Column {
         Row {
             Card {
                 Image(
                     painter = rememberImagePainter(meal?.imageUrl),
                     contentDescription = "Meal Image",
-                    modifier = Modifier.size(200.dp)
+                    modifier = Modifier
+                        .size(imageSizeDp)
+                        .padding(8.dp)
                 )
             }
-            Text(text = meal?.name ?: "Empty Meal")
+            Text(
+                text = meal?.name ?: "Empty Meal",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+            )
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Change state of meal profile picture")
+        Button(onClick = {
+            isExpanded = !isExpanded
+        }) {
+            Text(
+                text = "Change state of meal profile picture"
+            )
         }
     }
 }
