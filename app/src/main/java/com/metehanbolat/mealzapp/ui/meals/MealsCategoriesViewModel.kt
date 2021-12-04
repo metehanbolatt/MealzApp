b9a37c7e40ea270d25991d12a9547bf532db1642
metehanie <metehan.bolat.ie@gmail.com>
2021-12-05 00:24:25 +0300

    Working with my own scope in ViewModel.

--- app/src/main/java/com/metehanbolat/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/metehanbolat/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -1,11 +1,32 @@
 package com.metehanbolat.mealzapp.ui.meals
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.metehanbolat.mealzapp.model.MealsRepository
 import com.metehanbolat.mealzapp.model.response.MealResponse
+import kotlinx.coroutines.*
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()) : ViewModel() {
-    suspend fun getMeals() : List<MealResponse>{
+
+    private val mealsJob = Job()
+
+    init {
+        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
+        scope.launch() {
+            val meals = getMeals()
+            mealsState.value = meals
+        }
+    }
+
+    val mealsState =  mutableStateOf(emptyList<MealResponse>())
+
+    private suspend fun getMeals() : List<MealResponse>{
         return repository.getMeals().categories
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        mealsJob.cancel()
     }
 }
